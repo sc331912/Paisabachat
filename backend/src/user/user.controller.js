9ac23ee82e7c8218aa2736d0fa82a103118cc218
sc331912 <snehachand1912@gmail.com>
2026-02-08 23:14:34 +0530

    login user and set auth token in cookie

--- backend/src/user/user.controller.js
+++ backend/src/user/user.controller.js
@@ -1,4 +1,6 @@
 import UserModel from "./user.model.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 export const createUser= async (req,res) =>{
 
 try{
@@ -12,11 +14,38 @@ try{
 }
 }
 
+const createToken= async (user) =>{
+    const payload={
+        id: user._id,
+        fullname: user.fullname,
+        email: user.email,
+        role: user.role
+    }
+    const token= await jwt.sign(payload, process.env.AUTH_SECRET,{expiresIn:"1d"});
+    return token;
+}
+
 export const login= async (req,res) =>{
 
 try{
-    const data= req.body;
-    console.log(data);
+    const {email, password}= req.body;
+    const user= await UserModel.findOne({email});
+    if(!user){
+        return res.status(404).json({message:"User not found"});
+    }
+    const isLoged= await bcrypt.compare(password, user.password);
+    if(!isLoged){
+        return res.status(401).json({message:"Invalid credentials"});
+    }
+    const token= await createToken(user);
+    res.cookie("authToken", token,{
+        maxAge: 60*60*24*1000,
+        domain: process.env.ENVIRONMENT === "DEV" ? "localhost" : process.env.DOMAIN,
+         secure: process.env.ENVIRONMENT === "DEV" ? false : true,
+            httpOnly: true
+
+    });
+    res.json({message:"Login successful"});
 
 }catch(err){
     res.status(500).json({message:"Internal Server Error"});
