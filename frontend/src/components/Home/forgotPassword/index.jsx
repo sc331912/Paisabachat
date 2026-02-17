9d0ec69099b2e102f2992d6e2df29ec8bda4b718
sc331912 <snehachand1912@gmail.com>
2026-02-17 12:52:47 +0530

    fix: Correct typo in state initialization for token in forgot password component

--- frontend/src/components/Home/forgotPassword/index.jsx
+++ frontend/src/components/Home/forgotPassword/index.jsx
@@ -11,10 +11,12 @@ axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const { Item } = Form;
 
-const Login = () => {
+const ForgotPassword = () => {
   const navigate = useNavigate();
-const [loginForm] = Form.useForm();
+const [forgotForm] = Form.useForm();
+const [rePasswordForm] = Form.useForm();
 const [loading, setLoading] = useState(false);
+const [token, setToken] = useState(null);
 
 
   const onFinish = async (values) => {
@@ -69,13 +71,59 @@ if (role === "user") {
           </h1>
 
           <p className="text-center text-gray-300 mb-6 text-sm">
-            Smart way to track & save your money
+            Forgot your password? No worries! Enter your email below and we'll send you instructions to reset it.
           </p>
+          {
+            token ?
+            
+<Form name="login_form"
+          layout="vertical"
+          onFinish={onFinish}
+          form={rePasswordForm}>
 
+  <Item
+              name="password"
+              label={<span className="text-gray-300">Password</span>}
+              rules={[{ required: true, message: "Password required" }]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Enter your password"
+                className="bg-[rgba(0,0,0,0.6)] text-green-100 border-green-500/40 backdrop-blur-md"
+              />
+            </Item>
+            <Item
+              name="re-password"
+              label={<span className="text-gray-300">Re-Enter Password</span>}
+              rules={[{ required: true, message: "Password required" }]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                placeholder="Enter your password"
+                className="bg-[rgba(0,0,0,0.6)] text-green-100 border-green-500/40 backdrop-blur-md"
+              />
+            </Item>
+
+            {/* PASSWORD */}
+
+            {/* BUTTON */}
+            <Item>
+              <Button
+              loading={loading}
+                htmlType="submit"
+                block
+                className="!bg-green-500 hover:!bg-green-600 !text-black !font-bold h-11 rounded-lg shadow-[0_0_20px_rgba(0,255,150,0.6)]"
+              >
+                Change Password
+              </Button>
+            </Item>
+
+          </Form>
+          :
           <Form name="login_form"
           layout="vertical"
           onFinish={onFinish}
-          form={loginForm}>
+          form={forgotForm}>
 
             {/* EMAIL */}
             <Item
@@ -91,17 +139,6 @@ if (role === "user") {
             </Item>
 
             {/* PASSWORD */}
-            <Item
-              name="password"
-              label={<span className="text-gray-300">Password</span>}
-              rules={[{ required: true, message: "Password required" }]}
-            >
-              <Input.Password
-                prefix={<LockOutlined />}
-                placeholder="Enter your password"
-                className="bg-[rgba(0,0,0,0.6)] text-green-100 border-green-500/40 backdrop-blur-md"
-              />
-            </Item>
 
             {/* BUTTON */}
             <Item>
@@ -111,27 +148,28 @@ if (role === "user") {
                 block
                 className="!bg-green-500 hover:!bg-green-600 !text-black !font-bold h-11 rounded-lg shadow-[0_0_20px_rgba(0,255,150,0.6)]"
               >
-                Login to Paisa Bachat
+                Send Reset Instructions
               </Button>
             </Item>
 
           </Form>
+          }
 
           {/* LINKS */}
           <div className="flex items-center justify-between mt-2">
 
             <Link
-              to="/forgot-password"
+              to="/"
               className="text-gray-400 hover:text-green-400 underline"
             >
-              Forgot Password?
+              Sign in
             </Link>
 
             <Link
               to="/signup"
               className="text-green-400 font-semibold hover:text-green-300"
             >
-              Create Account →
+              Don't have an account? Create One →
             </Link>
 
           </div>
@@ -143,7 +181,7 @@ if (role === "user") {
   );
 };
 
-export default Login;
+export default ForgotPassword;
 
 
 
